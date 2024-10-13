1823644cf7883e36f30d74635c51e476fdce266e
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-13 18:25:23 +0530

    Update Queriz.py

--- pages/Queriz.py
+++ pages/Queriz.py
@@ -39,7 +39,7 @@ from langchain.output_parsers.openai_tools import JsonOutputToolsParser
 from langchain.output_parsers import PydanticOutputParser
 
 
-ICON_BLUE = r"C:\Users\utkar\Downloads\Lervis_logo.png"
+ICON_BLUE = "https://github.com/Utkarsh13tiwari/Lervis-Enterprise.ai/blob/main/transperent_logo.png"
 
 st.logo(ICON_BLUE, icon_image=ICON_BLUE)
 
