3dc3f57555b0a512449d6d0fb72d5daeeeb7bf79
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-16 16:05:46 +0530

    Update chat_with_csv.py

--- pages/chat_with_csv.py
+++ pages/chat_with_csv.py
@@ -18,7 +18,6 @@ st.logo(ICON_BLUE, icon_image=ICON_BLUE)
 # Set the page configuration
 st.set_page_config(
     page_title="Lervis Enterprise",
-    layout="wide",
     page_icon = ICON_BLUE
 ) 
 
