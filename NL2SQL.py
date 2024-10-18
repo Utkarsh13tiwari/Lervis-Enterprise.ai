40e1bf065a45c0fb53086e578c3de44ab9a6e0b0
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2024-10-18 13:18:55 +0530

    Update NL2SQL.py

--- NL2SQL.py
+++ NL2SQL.py
@@ -386,18 +386,23 @@ with col2:
                 df = pd.read_sql(query, conn)
 
             # Generate and display plot
-            plot_code = chain2.invoke({"query": user_input, "df": df})
-            st.code(plot_code["text"], language="python")
-            exec(plot_code["text"])  # Execute the code to generate a plot
+            try:
+                plot_code = chain2.invoke({"query": user_input, "df": df})
+                st.code(plot_code["text"], language="python")
+                exec(plot_code["text"])  # Execute the code to generate a plot
+                
+                # Check if 'fig' was created
+                if 'fig' in locals():
+                    st.session_state['Session'][st.session_state['current_Session']].append({"isUser": False, "text": "Generated Plot:", "plot": fig})
+                    st.plotly_chart(fig, key="user_generated_plot")
             
-            # Check if 'fig' was created
-            if 'fig' in locals():
-                st.session_state['Session'][st.session_state['current_Session']].append({"isUser": False, "text": "Generated Plot:", "plot": fig})
-                st.plotly_chart(fig, key="user_generated_plot")
-            
+            except Exception as e:
+                st.write(f"Could not plot  the data because: {e}")
+
             if len(st.session_state['Session'][st.session_state['current_Session']]) == 3:
                 print(len(st.session_state['Session'][st.session_state['current_Session']]))
                 st.rerun()
+
 
 
     # After each input, the messages are appended to the respective session’s chat history.
