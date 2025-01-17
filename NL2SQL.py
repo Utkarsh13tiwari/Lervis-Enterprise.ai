ffecb934abb3ecd162f61a7ca70fd31eefda7086
UTKARSH TIWARI <93029055+Utkarsh13tiwari@users.noreply.github.com>
2025-01-18 01:14:30 +0530

    Update NL2SQL.py

--- NL2SQL.py
+++ NL2SQL.py
@@ -76,9 +76,9 @@ st.markdown(
 #----------------------------------------------------------------------------------------------------------------------
 col1, col2, col3 = st.columns([1, 4, 1])
 #------------------------------------------------- llm's -----------------------------------------------------------------------------
-llm_gpt = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=openai)
+llm_gpt = ChatOpenAI(model="gpt-4o-2024-08-06", temperature=0, api_key=openai)
 #mixtral-8x7b-32768
-llm_gpt = ChatGroq(
+llm_groq = ChatGroq(
     model="llama3-70b-8192",
     temperature=0,
     max_tokens=None,  # Limiting the number of tokens per request
